cbe9b81a5cf8bb054b5fa4f10e4d10ff53725034
aaron <aaronye1001@gmail.com>
2025-02-16 08:31:02 -0500

    add sorting

--- src/app/api/create-incident/route.ts
+++ src/app/api/create-incident/route.ts
@@ -26,7 +26,9 @@ export async function POST(request: Request) {
         });
     }
 
-    await notifyEmergencyContacts(body.emergencyContacts, body.victimName, body.latitude || 43.6594719, body.longitude || -79.3978135);
+    const [lat, lng] = body.gpsCoordinates.split(" ").map(parseFloat);
+
+    await notifyEmergencyContacts(body.emergencyContacts, body.victimName, lat || 43.6594719, lng || -79.3978135);
 
     return NextResponse.json(incident);
 }
